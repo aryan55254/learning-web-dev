db8125b782abed64d56826554d5357a89a731f36
Aryan <aryanmishra55254@gmail.com>
2024-09-14 20:06:26 +0530

    week 1 complete

--- javascript/week1/learning/learn.js
+++ javascript/week1/learning/learn.js
@@ -1,10 +1,1 @@
-const a = "john";
-console.log(a);
-console.log("a");
-var is = true;
-is = "aryan";
-is = 10;
-console.log(is);
-//const doesnt change value
-let users = ["harry","olo","asd"];
-console.log(users[2]);
+
