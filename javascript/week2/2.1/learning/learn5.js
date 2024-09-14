4c225eb888cee3e9812e8933c0189dcf441c207f
Aryan <aryanmishra55254@gmail.com>
2024-09-14 23:00:00 +0530

    functional arguments

---
+++ javascript/week2/2.1/learning/learn5.js
@@ -0,0 +1,8 @@
+const data = fs.readFileSync('./a.txt','utf-8');
+console.log(data);
+
+const contents = fs.readFileSync('./b.txt','utf-8');
+console.log(contents);//read files sync
+
+const content = fs.readFile('./b.txt','utf-8');
+console.log(contents);//read files async
---
+++ javascript/week2/2.1/learning/learn7.js
@@ -0,0 +1,18 @@
+//functional arguments
+//passing a function to another function as argument
+function sum(a,b){
+    return a + b;
+}
+function multiply(a,b){
+    return a * b;
+}
+function subtract(a,b){
+    return a - b;
+}
+function divide(a,b){
+    return a/b;
+}
+function doOperation (a,b,op){
+    return op(a,b)
+}
+console.log(doOperation(1,2,divide));
