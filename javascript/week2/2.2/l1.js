3cd7813f4c990498f5260c7d80d79e8ccd0d660e
Aryan <aryanmishra55254@gmail.com>
2024-09-16 16:05:10 +0530

    classes.commit

---
+++ javascript/week2/2.2/l1.js
@@ -0,0 +1,24 @@
+//classes - way to define  blueprints for creating objects that are different from other objects defined in last section
+class Rectangle {
+    constructor(width, height, color) {
+         this.width = width;
+         this.height = height;
+         this.color = color; 
+    }
+    
+    area() {
+        const area = this.width * this.height;
+          return area;
+    }
+    
+    paint() {
+             console.log("color is");
+             console.log(this.color);
+    }
+    
+ }
+ 
+ const rect = new Rectangle(2, 4,"red")//new means instance/object of rectangle class
+ const area = rect.area();
+ console.log(area);
+ 
