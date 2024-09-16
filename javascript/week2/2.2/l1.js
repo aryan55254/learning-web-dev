c15fc1cb4591bba2ac02bffef4a45e661d0c8bed
Aryan <aryanmishra55254@gmail.com>
2024-09-16 20:48:25 +0530

    2.22

--- javascript/week2/2.2/l1.js
+++ javascript/week2/2.2/l1.js
@@ -12,14 +12,20 @@ class Rectangle {
     }
     
     paint() {
-             console.log("color is");
-             console.log(this.color);
+             const color = this.color;
+             return color;
     }
     
  }
  
- const rect = new Rectangle(2, 4,"red")//new means instance/object of rectangle class
+ const rect = new Rectangle(2, 4,"red");//new means instance/object of rectangle class
  const area = rect.area();
  const rang = rect.paint();
- console.log(rang);
+ 
  console.log(area);
+console.log(rang);
+ const rect2 = new Rectangle(3,4,"yellow");
+ const area2 = rect2.area();
+ const rang2 = rect2.paint();
+ console.log(area2);
+ console.log(rang2);
