20396779c31cfce6b5afefab01af7f2e97823542
Aryan <aryanmishra55254@gmail.com>
2024-09-17 22:13:49 +0530

    17/9-02

--- javascript/week1/1.js
+++ javascript/week1/1.js
@@ -1,1 +1,2 @@
 console.log("aryan");
+console.log("first js codes");
---
+++ javascript/week1/varaibles01.js
@@ -0,0 +1,10 @@
+//variables are containeers for data
+// let const var
+fullname = "tonystark"
+age = 24;
+x = null;//the value is empty
+y=undefined;//don't know what is in there
+console.log(fullname);
+console.log(age);
+console.log(x);
+console.log(y);
