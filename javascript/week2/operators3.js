c6f741bc326f7b07bd9d88ce418856d713955f81
Aryan <aryanmishra55254@gmail.com>
2024-09-18 21:32:40 +0530

    18/09-07

--- javascript/week2/operators3.js
+++ javascript/week2/operators3.js
@@ -1,1 +1,8 @@
 //comparision operators
+let a = 5;
+let b = 6;
+console.log("5==6",a==b);//answer in true and false
+console.log("5notequalto6",a!=b);//!=not
+ //strict equal to is === , it also checks datatype as ==lets vallue inside a string as value
+ //use triple equalto is used mostly
+ // <,>,>=,<=,
