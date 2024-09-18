98b7dbdcc545973a967d04a44bd139a4cbac74c3
Aryan <aryanmishra55254@gmail.com>
2024-09-18 19:52:39 +0530

    18/09-03

---
+++ javascript/week2/operators.js
@@ -0,0 +1,11 @@
+/*arithmetic operator
+,+,-,*,/,modulus,exponential,increment,decrement
+*/
+let a = 5;
+let b = 3;
+console.log("a + b=",a + b);
+console.log("a*b=",a*b);
+//modulus %= tells remainder
+console.log("a%b=",a%b);
+//exponential=power=**
+console.log
