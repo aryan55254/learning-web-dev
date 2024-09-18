ee8a35a628c16a61ae99b9ce7d4f85b8019fef22
Aryan <aryanmishra55254@gmail.com>
2024-09-18 18:33:48 +0530

    18/09-01

--- javascript/week2/letvarconst.js
+++ javascript/week2/letvarconst.js
@@ -1,3 +1,15 @@
-//let=can be re dclared and updates
-//can't be re declaed but can be updated
-//can't be redeclared or updated
+//var=can be re dclared and updates
+//let=can't be re declaed but can be updated
+//const = can't be redeclared or updated
+let fullname = "tony stark";
+let age ="20";
+age = 55;
+var h = 22;
+var h = 66;
+
+
+const u = 77;
+// can also do 
+let a;
+console.log(a);//value undifined , doesn't work in const
+//u can redefine let in new block-{}
