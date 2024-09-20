00b72548d2447e81e6c4d1ba03a1977b4af4199e
Aryan <aryanmishra55254@gmail.com>
2024-09-21 03:14:22 +0530

    21/09-01

--- javascript/day 7-8/for-ofloop.js
+++ javascript/day 7-8/for-ofloop.js
@@ -1,1 +1,8 @@
-//used for loops in string and arrays
+//for-of  loop =used for loops in string and arrays 
+let str = "aryan"
+let size=0;
+for (let i of str){//iterator
+console.log(i);
+size++;
+}
+console.log("string size=",size);
