0d0fe0fc4fdcd775819b6245262103e3b2dfb6c8
Aryan <aryanmishra55254@gmail.com>
2024-09-21 14:06:06 +0530

    21/09-03

--- javascript/sync and async/io.js
+++ javascript/sync and async/io.js
@@ -1,1 +1,7 @@
-//i/0 heavy operation
+//sync code this is 
+//loops are cpu bound task
+const fs =require("fs");//i/o bound task
+const contents = fs.readFileSync("./a.txt","utf-8");
+console.log("contents");//sync
+const contents = fs.readFile("./a.txt","utf-8");
+console.log("contents");//async
