2c9f2eba27ca070d53b43a3bedbee0bbacf8489f
Aryan <aryanmishra55254@gmail.com>
2024-09-22 14:54:20 +0530

    22/09-02

---
+++ javascript/day9-10/assignment03.js
@@ -0,0 +1,7 @@
+//create a game where you start with any random game number,ask the user to keep guessing the game no. until the user enters the correct value
+let gamenumber = 25;
+let usernumber = prompt("guess the game number:");
+while (usernumber == gamnumber){
+     let usernumber=prompt("you entered wrong number try again:")
+}
+console.log("congratulations,you entered right number");
