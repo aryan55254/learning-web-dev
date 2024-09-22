5c250cb7e60175d6bc209f4322141a8f15e0a9dc
Aryan <aryanmishra55254@gmail.com>
2024-09-23 01:03:21 +0530

    23/09-01

---
+++ javascript/string  assignments/assignment1.js
@@ -0,0 +1,3 @@
+let fullname = prompt("enter you full name:");
+let username = "@" + fullname + fullname.length ;
+console.log(username);
---
+++ javascript/strings/stringmethods1.js
@@ -0,0 +1,7 @@
+// string methods are in built functions to manipulate a string
+let x = "aryan";
+let y = x.toUpperCase();
+console.log(y);
+//strings are originally immutable
+//str.trim() removes whitespace
+//concat add two strings can be done by just +
