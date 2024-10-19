2342eb7c47d09f804565a5256f63c0a4def3a883
Aryan <aryanmishra55254@gmail.com>
2024-10-19 21:16:42 +0530

    19/10-03

---
+++ javascript/DOM MANIPULATION/eventlisteners/script.js
@@ -0,0 +1,22 @@
+/*const divs = document.querySelectorAll('div');
+
+// Add a click event listener to each div
+divs.forEach(div => {
+    div.addEventListener('click', () => {
+        div.remove(); // Remove the clicked div
+    });
+});*/
+
+const x = document.getElementById("1");
+x.addEventListener("click", () => x.remove());
+
+const y = document.getElementById("2");
+y.addEventListener("click", () => y.remove());
+
+const w = document.getElementById("3");
+w.addEventListener("click", () => w.remove());
+
+const z = document.getElementById("4");
+z.addEventListener("click", () => z.remove());
+
+
